8d83fc74f42b268a3e9252be3d20fd623b97798c
rdtill <37480735+rdtill@users.noreply.github.com>
2018-12-05 21:12:28 -0500

    added gathering of summary statistics
    Changed the script so files are no longer kept in RAM, and instead are removed after being processed.

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -1,18 +1,36 @@
-#initial exploratory analysis
-#setwd('C:\\Users\\tillr\\Coursera\\Capstone\\data\\en_US')
-setwd('C:\\Users\\User\\Desktop\\Coursera\\Data Science Specialization\\Capstone\\final\\en_US')
-
+#####
+#    installing packages
+#####
 library(NLP)
 library(readr)
 
-#    reading in data
 #####
-#raw data
-dat_blogs   = read_lines('en_US.blogs.txt')
-dat_news    = read_lines('en_US.news.txt')
-dat_twitter = read_lines('en_US.twitter.txt')
+#    exploratory analysis
+#####
 #reference data
 setwd('C:\\Users\\User\\Desktop\\Coursera\\Data Science Specialization\\Capstone\\ref_dat')
 prof_en    = read_lines('prof_en.txt')
+
+#setwd('C:\\Users\\tillr\\Coursera\\Capstone\\data\\en_US')
+setwd('C:\\Users\\User\\Desktop\\Coursera\\Data Science Specialization\\Capstone\\final\\en_US')
+#character & word count summary
+files      = dir()
+summ_char  = list()
+summ_word = list()
+for(i in 1:length(files)) {
+  print(paste('    Processing ', files[i], sep = ''))
+  
+  tmp_file        = read_lines(files[i])
+  tmp_char        = sapply(tmp_file, nchar)
+  tmp_word        = sapply(tmp_file, FUN = function(txt) {
+                      length(strsplit(txt, " ")[[1]])
+                    })
+  summ_char[[i]]  = summary(tmp_char)
+  summ_word[[i]]  = summary(tmp_word)
+  names(summ_char)[i] = files[i]
+  names(summ_word)[i] = files[i]
+  
+  remove(tmp_file, tmp_char, tmp_word)
+}
 
 
