b6fa4959a6ad5ac1e800bc3db4a2dda59492383c
rdtill <37480735+rdtill@users.noreply.github.com>
2018-12-17 13:28:00 -0500

    Add files via upload

--- ngram generation/ngrams_analysis.R
+++ ngram generation/ngrams_analysis.R
@@ -16,7 +16,7 @@ source('functions.R')
 setwd('C:\\Users\\tillr\\Coursera\\Capstone\\project\\ngram generation\\ngrams')
 load("bigrams.Rdata")
 
-#load("bigrams.Rdata")
+#blogs
 bi_blog = lapply(bigrams$en_US.blogs.txt, FUN = function(ele) {
   if(length(ele) != 0) {
     for(i in 1:length(ele)) {
@@ -31,6 +31,51 @@ bi_blog = lapply(bigrams$en_US.blogs.txt, FUN = function(ele) {
     0
   }
 })
+for(i in 1:length(bi_blog)) {
+  
+}
+bi_df = 0
+for(i in 1:length(bi_blog)) {
+  if(is.data.frame(bi_blog[[i]])) {
+    if(!is.data.frame(bi_df)) {
+      bi_df = bi_blog[[i]]
+    } else {
+      bi_df = rbind(bi_df, bi_blog[[i]])
+    }
+  }
+}
+
+#news
+# bi_news = lapply(bigrams$en_US.news.txt, FUN = function(ele) {
+#   if(length(ele) != 0) {
+#     for(i in 1:length(ele)) {
+#       if(i == 1) {
+#         output = data.frame(first = ele[[i]][1], second = ele[[i]][2])
+#       } else {
+#         output = rbind(output, data.frame(first = ele[[i]][1], second = ele[[i]][2]))
+#       }
+#     }
+#     output
+#   } else {
+#     0
+#   }
+# })
+# 
+# #twtr
+# bi_twtr = lapply(bigrams$en_US.twitter.txt, FUN = function(ele) {
+#   if(length(ele) != 0) {
+#     for(i in 1:length(ele)) {
+#       if(i == 1) {
+#         output = data.frame(first = ele[[i]][1], second = ele[[i]][2])
+#       } else {
+#         output = rbind(output, data.frame(first = ele[[i]][1], second = ele[[i]][2]))
+#       }
+#     }
+#     output
+#   } else {
+#     0
+#   }
+# })
 
 
 
